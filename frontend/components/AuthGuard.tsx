8325807febc07705aba4786f7057af94641c134e
anshiky73-svg <anshiky73@gmail.com>
2026-04-03 01:22:12 +0530

    fix: authgaurd.tsx

--- frontend/components/AuthGuard.tsx
+++ frontend/components/AuthGuard.tsx
@@ -1,19 +1,24 @@
 "use client"
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
 
     const router = useRouter()
+    const [isChecking, setIsChecking] = useState(true)
 
     useEffect(() => {
         const token = localStorage.getItem("token")
 
         if (!token) {
             router.push("/login")
+        } else {
+            setIsChecking(false)
         }
-    }, [])
+    }, [router])
+
+    if (isChecking) return null
 
     return <>{children}</>
 }
