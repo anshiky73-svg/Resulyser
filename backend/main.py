a76d251dee304ce44647007e1c495a6be102d9b2
anshiky73-svg <anshiky73@gmail.com>
2026-04-16 21:33:23 +0530

    added frontend url in cors

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://resulyser.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
